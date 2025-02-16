3ed035127859ccae40feada9806fad2f260e18f5
Vikas Sharma <vikaskolhua20@gmail.com>
2025-02-16 19:55:08 +0530

    first_class

---
+++ class_practice.cpp
@@ -0,0 +1,18 @@
+# include<bits/stdc++.h>
+using namespace std;
+class fruit {
+   public:
+    string name;
+    int price;
+};
+int main()
+{
+    cout<<"My Name is Vikas Sharma"<<endl;
+    fruit f1;
+    f1.name="Mango";
+    f1.price = 150;
+    cout<<f1.name<<endl;
+    cout<<f1.price<<endl;
+    cout<<"HI"<<endl;
+    return 0;
+}
